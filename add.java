ab26e11b59ec79fe28509f3915d13240669deb6c
thiyasree <thiyaasree110@gmail.com>
2024-12-07 12:59:04 +0530

    Update add.java

--- add.java
+++ add.java
@@ -4,9 +4,9 @@ class add
 public static void main (String args[])
 {
 Scanner myobj=new scanner(System.in);
-System.out.println("enter any number:");
+System.out.println("enter number:");
 int first=myobj.nextint();
-System.out.println("enter any number:");
+System.out.println("enter number:");
 int second=myobj.nextint();
 int add=first+second;
 System.out.println("the sum is:"+add);
