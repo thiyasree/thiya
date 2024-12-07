dcfc8b48880acbec9f00e49bab021d0478f59bab
thiyasree <thiyaasree110@gmail.com>
2024-12-07 12:02:55 +0530

    push

---
+++ add.java
@@ -0,0 +1,14 @@
+import java.unit.Scanner 
+class add
+{
+public static void main (String args[])
+{
+Scanner myobj=new scanner(System.in);
+System.out.print("enter any number:");
+int first=myobj.nextint();
+System.out.print("enter any number:");
+int second=myobj.nextint();
+int add=first+second;
+System.out.println("the sum is:"+add);
+}
+}
